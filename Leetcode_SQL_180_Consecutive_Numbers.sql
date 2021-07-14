0e1cbc5e695b871c16d9bde225668cce1b98b749
Der-Hsuan Tsou <83623711+tiatsou@users.noreply.github.com>
2021-07-14 19:00:04 -0400

    Update Leetcode_SQL_180_Consecutive_Numbers.sql

--- Leetcode_SQL_180_Consecutive_Numbers.sql
+++ Leetcode_SQL_180_Consecutive_Numbers.sql
@@ -23,12 +23,12 @@ VALUES
 -- March 2021 Solution
 SELECT L1.Num AS ConsecutiveNums
 FROM Logs AS L1
-INNER JOIN Logs AS L2 ON L2.id+1 = L1.id AND L1.Num = L2.Num
-INNER JOIN Logs AS L3 ON L3.id+2 = L1.id AND L2.Num = L3.Num;
+INNER JOIN Logs AS L2 ON L2.id + 1 = L1.id AND L1.Num = L2.Num
+INNER JOIN Logs AS L3 ON L3.id + 2 = L1.id AND L2.Num = L3.Num;
 
 -- July 2021 Solution
 SELECT L1.Num AS ConsecutiveNums
 FROM Logs L1
-INNER JOIN Logs L2 ON L1.Id = L2.Id+1
-INNER JOIN Logs L3 ON L1.Id = L3.Id+2
+INNER JOIN Logs L2 ON L1.Id = L2.Id + 1
+INNER JOIN Logs L3 ON L1.Id = L3.Id + 2
 WHERE L1.Num = L2.Num AND L1.Num = L3.Num;
