7e7a1876a53a778797d7dd2a9c8c06ae10bfec70
Der-Hsuan Tsou <83623711+tiatsou@users.noreply.github.com>
2021-08-23 13:04:42 -0400

    Add files via upload

---
+++ Leetcode_SQL_571_Find_Median_Given_Frequency_of_Numbers.sql
@@ -0,0 +1,52 @@
+--Problem Number & Name: 571. Find Median Given Frequency of Numbers
+
+--Data Preparation
+CREATE TABLE Numbers (
+  `Number` INTEGER,
+  `Frequency` INTEGER
+);
+
+INSERT INTO Numbers
+  (`Number`, `Frequency`)
+VALUES
+  ('0', '3'),
+  ('1', '3'),
+  ('2', '5'),
+  ('3', '1');
+
+/*
+August 2021 Solution
+Thoughts: CTE
+*/
+WITH Main AS
+(
+  SELECT *, SUM(Frequency) OVER(ORDER BY Number ROWS BETWEEN UNBOUNDED
+                                PRECEDING AND CURRENT ROW) AS Accu 
+  FROM Numbers
+)
+SELECT AVG(Number) AS median
+FROM Main
+WHERE Accu 
+BETWEEN 
+(SELECT SUM(Frequency) / 2 FROM Numbers) 
+AND 
+(SELECT SUM(Frequency) / 2 FROM Numbers) + Frequency;
+
+/*
+April 2021 Solution
+*/
+SELECT AVG(Number) AS Median
+FROM 
+(
+SELECT Number, Frequency, accu_num, ct
+FROM
+  (
+    SELECT *, SUM(Frequency) OVER(ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS accu_num 
+    FROM Numbers
+  ) AS subq1,
+  (
+    SELECT SUM(Frequency) AS ct
+    FROM Numbers
+  ) AS subq2
+) AS subq
+WHERE accu_num BETWEEN FLOOR(ct / 2) AND FLOOR(ct / 2) + Frequency;
